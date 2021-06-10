10705e3a4b9657fb9ea2db0aa45aac3f0fa04d16
Ameya Bhagat <techliteral@outlook.com>
2021-06-10 13:46:58 +0530

    Initial Commit

---
+++ Source/View Controllers/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  Signary
+//
+//  Created by Ameya Bhagat on 10/06/21.
+//
+
+import UIKit
+
+class HomeViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - TABLEVIEW DATA SOURCE METHODS
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
