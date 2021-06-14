7c69d549c94967f899aed42f184ab588c6a4a4ce
Ameya Bhagat <techliteral@outlook.com>
2021-06-14 15:03:36 +0530

    Minor Changes

---
+++ Source/Models/Core Data/ColorValueTransformer.swift
@@ -0,0 +1,27 @@
+//
+//  ColorValueTransformer.swift
+//  Signary
+//
+//  Created by Ameya Bhagat on 14/06/21.
+//
+
+import UIKit
+
+// 1. Subclass from `NSSecureUnarchiveFromDataTransformer`
+@objc(UIColorValueTransformer)
+final class ColorValueTransformer: NSSecureUnarchiveFromDataTransformer {
+
+    /// The name of the transformer. This is the name used to register the transformer using `ValueTransformer.setValueTrandformer(_"forName:)`.
+    static let name = NSValueTransformerName(rawValue: String(describing: ColorValueTransformer.self))
+
+    // 2. Make sure `UIColor` is in the allowed class list.
+    override static var allowedTopLevelClasses: [AnyClass] {
+        return [UIColor.self]
+    }
+
+    /// Registers the transformer.
+    public static func register() {
+        let transformer = ColorValueTransformer()
+        ValueTransformer.setValueTransformer(transformer, forName: name)
+    }
+}
